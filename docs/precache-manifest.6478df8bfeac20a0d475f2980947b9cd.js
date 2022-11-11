562a8a0498c21c75b8a939a666f2f3323dbd233a
Vasyl Sydorchuk <vscorporation@maol.ru>
2022-11-11 18:03:09 +0200

    githyub-pages-build

---
+++ docs/precache-manifest.6478df8bfeac20a0d475f2980947b9cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06e0a27f44e7fa7bf6558cd897ca82cf",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "f4011f18c8f7ca12dbdb",
+    "url": "/currency-converter/static/css/2.1bd241f3.chunk.css"
+  },
+  {
+    "revision": "110a9a1d60654a7a4f7e",
+    "url": "/currency-converter/static/css/main.7322ec51.chunk.css"
+  },
+  {
+    "revision": "f4011f18c8f7ca12dbdb",
+    "url": "/currency-converter/static/js/2.d123f41d.chunk.js"
+  },
+  {
+    "revision": "dd8cda50eb74a353854ff0299f406dbe",
+    "url": "/currency-converter/static/js/2.d123f41d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "110a9a1d60654a7a4f7e",
+    "url": "/currency-converter/static/js/main.d6ec40eb.chunk.js"
+  },
+  {
+    "revision": "eb143004aab74042b0e9",
+    "url": "/currency-converter/static/js/runtime-main.f38c1aee.js"
+  }
+]);
